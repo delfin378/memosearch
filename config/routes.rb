cdc3ea0c2d22f1c146f9d5999d0f6df86d3a0b56
delfin378 <type0bs02hd@yahoo.co.jp>
2020-06-17 11:06:17 +0900

    トップページ 雛形

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  get 'messages/index'
+  root to: 'messages#index'
   get 'users/index'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
